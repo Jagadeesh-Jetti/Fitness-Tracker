b2a30d932e73e8a8279544badce4c4e9225f466f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-11-30 01:03:36 +0530

    feat: initialized goal component

---
+++ src/pages/Goal/goal.jsx
@@ -0,0 +1,17 @@
+export const Goal = () => {
+  return (
+    <div>
+      <div>
+        <h1>Goal</h1>
+        <div>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Description" />
+          <input type="text" placeholder="Target Date" />
+          <input type="text" placeholder="Target Calories" />
+          <input type="text" placeholder="status" />
+          <button> Add Goal </button>
+        </div>
+      </div>
+    </div>
+  );
+};
