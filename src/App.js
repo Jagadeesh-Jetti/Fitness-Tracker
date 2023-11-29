a42b69cb9e6a0e96621473260fba4006a06eb513
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-11-29 18:43:27 +0530

    feat: added routes for routing components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import { Navbar } from './components/Navbar/navbar.jsx';
+import { Dashboard } from './pages/Dashboard/dashboard.jsx';
+import { Exercises } from './pages/Exercises/exercises.jsx';
+import { Goal } from './pages/Goal/goal.jsx';
+import { Food } from './pages/Food/food.jsx';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Fitness Tracker</h2>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/exercises" element={<Exercises />} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/goal" element={<Goal />} />
+        </Routes>
+      
       </header>
     </div>
   );
