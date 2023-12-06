de736c34f44f7f7bb2207bd0b5a6296f2f27a880
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-07 02:27:46 +0530

    feat: built goals component

--- src/pages/Goal/goal.jsx
+++ src/pages/Goal/goal.jsx
@@ -1,16 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGoals } from "../../actions";
+
 export const Goal = () => {
+  const dispatch = useDispatch();
+  const goals = useSelector((state) => state.goals);
+  console.log(goals);
+
+  useEffect(() => {
+    dispatch(fetchGoals);
+  }, [dispatch]);
+
   return (
-    <div>
+    <div className="goals_main">
+      <h1>Goal</h1>
       <div>
-        <h1>Goal</h1>
-        <div>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Description" />
-          <input type="text" placeholder="Target Date" />
-          <input type="text" placeholder="Target Calories" />
-          <input type="text" placeholder="status" />
-          <button> Add Goal </button>
-        </div>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Description" />
+        <input type="text" placeholder="Target Date" />
+        <input type="text" placeholder="Target Calories" />
+        <input type="text" placeholder="status" />
+        <button> Add Goal </button>
+      </div>
+      <div>
+        {goals.map((goal) => (
+          <div>
+            <p> {goal.name} </p>
+          </div>
+        ))}
       </div>
     </div>
   );
