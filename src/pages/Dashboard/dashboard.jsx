11d6e73442ea4ba695b24ecb457b0f6e616cc75b
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-07 02:28:07 +0530

    feat: initialized basic dashboard page

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchExercises, fetchFood, fetchGoals } from "../../actions";
+
 export const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchExercises());
+    dispatch(fetchFood());
+    dispatch(fetchGoals());
+  }, [dispatch]);
+
   return (
     <div>
       <div>
