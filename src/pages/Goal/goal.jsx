eeab6df9d2a9f8c954c8420436a24a9df81a426f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-08 04:53:43 +0530

    feat: added goals display section, add goal functionality

--- src/pages/Goal/goal.jsx
+++ src/pages/Goal/goal.jsx
@@ -1,33 +1,123 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import "../Goal/goal.css";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchGoals } from "../../actions";
+import { addGoal, deleteGoal, fetchGoals } from "../../actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export const Goal = () => {
   const dispatch = useDispatch();
   const goals = useSelector((state) => state.goals);
-  console.log(goals);
+  const [goalInput, setGoalInput] = useState({
+    name: "",
+    description: "",
+    createdAt: "",
+    calories: "",
+    status: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setGoalInput((prev) => ({ ...prev, [name]: value }));
+    console.log(goalInput);
+  };
+
+  const handleSubmit = () => {
+    dispatch(addGoal(goalInput));
+    setGoalInput({
+      name: "",
+      description: "",
+      createdAt: "",
+      calories: "",
+      status: "",
+    });
+  };
+
+  const handleDelete = (id) => {
+    dispatch(deleteGoal(id));
+  };
 
   useEffect(() => {
-    dispatch(fetchGoals);
+    dispatch(fetchGoals());
   }, [dispatch]);
 
   return (
     <div className="goals_main">
-      <h1>Goal</h1>
-      <div>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Description" />
-        <input type="text" placeholder="Target Date" />
-        <input type="text" placeholder="Target Calories" />
-        <input type="text" placeholder="status" />
-        <button> Add Goal </button>
+      <div className="goals_input_div">
+        <div className="page_header">Add Goal</div>
+        <div>
+          <input
+            type="text"
+            name="name"
+            value={goalInput.name}
+            onChange={(e) => handleInput(e)}
+            placeholder="Name"
+          />
+          <input
+            type="text"
+            name="description"
+            value={goalInput.description}
+            onChange={(e) => handleInput(e)}
+            placeholder="Description"
+          />
+        </div>
+        <div>
+          <input
+            type="date"
+            name="createdAt"
+            value={goalInput.createdAt}
+            onChange={(e) => handleInput(e)}
+            placeholder="Target Date"
+          />
+
+          <input
+            type="number"
+            name="calories"
+            value={goalInput.calories}
+            onChange={(e) => handleInput(e)}
+            placeholder="Target Calories"
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            name="status"
+            value={goalInput.status}
+            onChange={(e) => handleInput(e)}
+            placeholder="Status"
+          />
+          <button onClick={handleSubmit}>Add Goal</button>
+        </div>
       </div>
-      <div>
-        {goals.map((goal) => (
-          <div>
-            <p> {goal.name} </p>
-          </div>
-        ))}
+
+      <div className="goals_display">
+        <div className="page_header">Goals</div>
+        <div className="goals_display_layout">
+          {goals.length > 0 ? (
+            goals.map((goal) => (
+              <div key={goal._id} className="goals_display_child">
+                <div className="goals_content">
+                  <p>Name: {goal?.name}</p>
+                  <p>Description: {goal?.description}</p>
+                  <p>
+                    Target Date: {new Date(goal?.createdAt).toLocaleString()}
+                  </p>
+                  <p>Target Calories: {goal?.calories}</p>
+                  <p>Status: {goal?.status}</p>
+                </div>
+                <div className="goals_icon">
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    onClick={() => handleDelete(goal?._id)}
+                  />
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No goals added</p>
+          )}
+        </div>
       </div>
     </div>
   );
